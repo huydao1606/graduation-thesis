eddc6b686d1ef4158bfcbde6a0b0faf1de211380
tiesen243 <ttien56906@gmail.com>
2026-09-09 06:16:35 +0700

    chore: minor changes

--- apps/web/src/routes/dashboard/_components/schedule/index.tsx
+++ apps/web/src/routes/dashboard/_components/schedule/index.tsx
@@ -48,6 +48,7 @@ export const Schedules: React.FC<{
           startDate={query.startDate}
           endDate={query.endDate}
           setWeek={setQuery}
+          deviceId={deviceId}
         />
 
         <ScheduleSkeleton />
